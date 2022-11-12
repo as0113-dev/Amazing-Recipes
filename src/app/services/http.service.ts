b63d736e853d972faed379c328a49a766c32ea3a
Abdullah <ashehata1308@gmail.com>
2022-11-12 02:08:32 -0500

    subscribed to recipes api, and did testing to make sure able to get response with angular HttpClientModule, and added a http service to handle requests

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,31 @@
+import { environment } from "src/environments/environment";
+
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HttpService {
+    constructor(private http: HttpClient) { }
+
+    baseUrl = environment.BASEURL;
+    httpHeaders = {
+        headers: new HttpHeaders({
+            'X-RapidAPI-Key': environment.API_KEY,
+            'X-RapidAPI-Host': environment.HOST
+        })
+    }
+
+    getRecipesList() {
+        this.http.get(this.baseUrl + 'recipes/list?from=0&size=10&tags=under_30_minutes', this.httpHeaders).subscribe({
+            next: (resp) => {
+                console.log(resp);
+
+            },
+            error: (err) => {
+                console.log(err);
+            }
+        });
+    }
+}
